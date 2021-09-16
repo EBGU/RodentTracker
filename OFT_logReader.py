caee11739718e4bddbad3d11ecf77f3f8931cae5
Yinjun Harold JIA <34360222+EBGU@users.noreply.github.com>
2021-09-16 15:34:00 +0800

    Update OFT_logReader.py

--- OFT_logReader.py
+++ OFT_logReader.py
@@ -6,81 +6,109 @@ from multiprocessing import Pool
 import matplotlib.pyplot as plt
 from scipy.stats import ks_2samp as ks_test
 from scipy import stats 
+import psutil
+from multiprocessing import Pool
 
 home = 'yourFolder' 
 logDir = home + '/log'
 bin_num =50
 maxlen = 100
-
+totalTime = 0
 fileList = os.listdir(logDir)
+multiThread = psutil.cpu_count(False)
 
-ctrlDist = []
-expDist = []
+
 ctrlBin = []
 expBin = []
-for f in fileList:
-    distanceM = []
-    with open(os.path.join(logDir,f)) as l:
-        for i, lines in enumerate(l.readlines()):
-            if i >=3:
-                dist = float(lines.split('\t')[7])
-                distanceM.append(dist)   
-    if not('_1b' in f):
-        print(f)
-        ctrlDist.append(distanceM)
-        ctrlBin.append(np.histogram(distanceM,bins=bin_num,range=(0,maxlen),density=True)[0])
 
+class logReader():
+    def __init__(self,logDir):
+        self.logDir = logDir
+    def __call__(self,f):
+        accDistr = []
+        distanceM = []
+        with open(os.path.join(logDir,f)) as l:
+            for i, lines in enumerate(l.readlines()):
+                if i >=3:
+                    dist = float(lines.split('\t')[7])
+                    distanceM.append(dist)   
+                    accDistr.append(np.histogram(distanceM,bins=bin_num,range=(0,maxlen),density=True)[0])     
+        return accDistr
+
+Reader = logReader(logDir)
+    
+with Pool(multiThread) as p:
+    Bin = p.map(Reader,fileList)
+
+for i in Bin:
+    if totalTime == 0:
+        totalTime = len(i)
     else:
-        expDist.append(distanceM)
-        expBin.append(np.histogram(distanceM,bins=bin_num,range=(0,maxlen),density=True)[0])
+        totalTime = min(totalTime,len(i))
 
+ctrlBin = []
+expBin = []
+for i,j in zip(Bin,fileList):
+    if '_KO' in j:
+        print(j)
+        expBin.append(i[0:totalTime])
+    else:
+        ctrlBin.append(i[0:totalTime])
 
-ctrlDist = np.concatenate(ctrlDist)
-expDist = np.concatenate(expDist)
 ctrlBin = np.array(ctrlBin)
 expBin = np.array(expBin)
 
+class pValue():
+    def __init__(self,bin_num,totalTime,expBin,ctrlBin):
+        self.bin_num = bin_num
+        self.totalTime = totalTime
+        self.expBin = expBin
+        self.ctrlBin = ctrlBin
+    def __call__(self,x):
+        i = x // self.bin_num
+        j = x % self.bin_num
+        try:
+            p = stats.mannwhitneyu(ctrlBin[:,i,j],expBin[:,i,j])[1]
+        except:
+            p = 1  
+        return p      
+pCal = pValue(bin_num,totalTime,expBin,ctrlBin)
 
-def violin(data):
-    fig = plt.figure()
-    ax2 = plt.subplot(111)
-    parts = ax2.violinplot(data,widths=0.85,showmeans=False, showmedians=False,showextrema=False)
-    for pc in parts['bodies']:
-        pc.set_facecolor('#D43F3A')
-        pc.set_edgecolor('black')
-        pc.set_alpha(1)
-    mid = np.array([np.percentile(x, [25, 50, 75]) for x in data]).transpose()
-    means = [np.mean(x) for x in data]
-    quartile1, medians, quartile3 = mid
-    inds = np.arange(1, len(medians) + 1)
-    ax2.scatter(inds, medians, marker='o', color='white', s=30, zorder=3)
-    ax2.scatter(inds, means, marker='_', color='white', s=50, zorder=3)
-    ax2.vlines(inds, quartile1, quartile3, color='k', linestyle='-', lw=5)
-    plt.show()
-    return True
-
-
-
-print(ks_test(ctrlDist,expDist))
-print(stats.mannwhitneyu(ctrlDist,expDist))
-violin([ctrlDist,expDist])
+with Pool(multiThread) as p:
+    pPlot = np.array(p.map(pCal,range(bin_num*totalTime))).reshape(totalTime,bin_num)
 
 
-plt.hist(ctrlDist,bins=bin_num,density = True,cumulative=False,color='blue',alpha =0.5)
-plt.hist(expDist,bins=bin_num,density = True,cumulative=False,color='red',alpha =0.5)
+pPlot =np.where(pPlot<0.05,-np.log10(pPlot),0)
+
+
+
+dist = np.linspace(0.0, maxlen, num=bin_num, endpoint=False)+maxlen*0.5/bin_num
+t = np.arange(0,totalTime)
+fig,(ax1,ax2) = plt.subplots(2,1)
+expDist = np.mean(expBin,axis=0)
+ctrlDist = np.mean(ctrlBin,axis=0)
+im = ax1.pcolormesh(t, dist, np.mean(expBin,axis=0).transpose(),cmap='Reds',alpha=0.5)
+im = ax1.pcolormesh(t, dist, np.mean(ctrlBin,axis=0).transpose(),cmap='Blues',alpha=0.5)
+fig.colorbar(im, ax=ax1)
+im2 = ax2.pcolormesh(t, dist,pPlot.transpose())
+fig.colorbar(im2, ax=ax2)
 plt.show()
+
+
+timePoint = int(input('Please choose a time point :'))
+
 
 t = np.linspace(0.0, maxlen, num=bin_num, endpoint=False)+maxlen*0.5/bin_num
 
-mean = np.mean(ctrlBin,axis=0)
-std = stats.sem(ctrlBin,axis=0) # use 3 times sem 
+mean = np.mean(ctrlBin[:,timePoint,:],axis=0)
+std = stats.sem(ctrlBin[:,timePoint,:],axis=0) # use 3 times sem 
 plt.plot(t, mean,color="#0000FF",alpha=0.5)
 plt.fill_between(t,mean-std,mean+std, color="#9999FF",alpha=0.5) 
 #plt.show()
 
 
-mean = np.mean(expBin,axis=0)
-std = stats.sem(expBin,axis=0)  
+mean = np.mean(expBin[:,timePoint,:],axis=0)
+std = stats.sem(expBin[:,timePoint,:],axis=0)  
 plt.plot(t, mean,color="#FF0000",alpha=0.5)
 plt.fill_between(t,mean-std,mean+std, color="#FF9999",alpha=0.5) 
 
@@ -88,15 +116,13 @@ plt.fill_between(t,mean-std,mean+std, color="#FF9999",alpha=0.5)
 sig = []
 for i in range(bin_num):
     try:
-        p = stats.mannwhitneyu(ctrlBin[:,i],expBin[:,i])[1]
+        p = stats.mannwhitneyu(ctrlBin[:,timePoint,i],expBin[:,timePoint,i])[1]
     except:
         p = 1
     sig.append(p)
     if p<0.05:
-        print(ctrlBin[:,i])
-        print(expBin[:,i])
         print(t[i],p)
 sig = np.array(sig)
-plt.fill_between(t, 0, 0.06, where=sig < 0.05,color='green', alpha=0.5)
+plt.fill_between(t, 0, 0.1, where=sig < 0.05,color='green', alpha=0.5)
 plt.legend()
 plt.show()
